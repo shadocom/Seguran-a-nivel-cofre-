daa4d27805212c8a99a63b8e7d8b3e0ab6fca722
Gabriel Pilger <gabrielsupermon@gmail.com>
2020-11-24 16:05:30 -0300

    primeira_camera

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-#iniciando projeto
+import cv2 
+import numpy as np
+import os
+
+
+camera = cv2.VideoCapture(0)
+
+while True:
+    _, frame = camera.read()
+    cv2.imshow("Imagem", frame)
+
+    k = cv2.waitKey(60)
+    if k == 27:
+        break
