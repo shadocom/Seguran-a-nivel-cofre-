869e1771be2135b233a1b7a0be9837a7c39aca07
Guilherme Dalazen <guilhermebatistellad@gmail.com>
2020-11-27 20:34:51 -0300

    Adicionando contorno

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ while True:
     _, frame = camera.read()
     framehsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
 
-    menorverde = np.array[57,147,129]
-    maiorverde = np.array[255,255,255]
+    menorverde = np.array([57,147,129])
+    maiorverde = np.array([255,255,255])
 
     maskverde = cv.inRange(framehsv,menorverde,maiorverde)
     resultadoverde = cv.bitwise_and(frame,frame,mask=maskverde)
@@ -17,10 +17,17 @@ while True:
     _, threshverde = cv.threshold(framecinzaverde,3,255,cv.THRESH_BINARY)
     contornosverde, _ = cv.findContours(threshverde, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
-    afericao = cv.rectangle(contornosverde)
+    for contorno in contornosverde:
+        (x, y, w, h) = cv.boundingRect(contorno)
+        area = cv.contourArea(contorno)
+        
+        if area > 1000:
+            cv.putText(frame, "Verde detectado", (10, 50),
+                        cv.FONT_HERSHEY_SIMPLEX, 1, (255,255,255))
+            cv.drawContours(frame, contorno, -1, (0, 0, 0), 5)
+            cv.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
     cv.imshow("Imagem", frame)
-    cv.imshow("Imagemhsv",framehsv)
     k = cv.waitKey(60)
     if k == 27:
         break
